2c524e878356b0a2bf8f5285a088d6fe41588863
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-11-22 16:07:44 +0000

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,21 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    Vector2 moveInput;
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 10f;
-    Vector2 moveInput;
+    
+
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
 
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -42,11 +46,12 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+
         if (value.isPressed)
         {
             myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
         }
-
     }
 
     void FlipSprite()
