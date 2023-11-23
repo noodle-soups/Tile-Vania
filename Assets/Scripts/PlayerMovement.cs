8c43ccf8c0496be3eb8a69dfce2ad5c13c3c9ead
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-11-23 10:43:32 +0000

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+
 //using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,17 +16,19 @@ public class PlayerMovement : MonoBehaviour
     float gravityDefault;
 
 
-    Rigidbody2D myRigidbody;
-    Animator myAnimator;
+    Rigidbody2D rb;
+    Animator anim;
     CapsuleCollider2D myCapsuleCollider;
+
+    bool ladderLatch = false;
 
 
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         myCapsuleCollider = GetComponent<CapsuleCollider2D>();
-        gravityDefault = myRigidbody.gravityScale;
+        gravityDefault = rb.gravityScale;
     }
 
     void Update()
@@ -41,43 +45,55 @@ public class PlayerMovement : MonoBehaviour
 
     void Run()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.velocity.y);
-        myRigidbody.velocity = playerVelocity;
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
 
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // true when player is moving R/L
-        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon; // true when player is moving R/L
+        anim.SetBool("isRunning", playerHasHorizontalSpeed);
     }
 
     void OnJump(InputValue value)
     {
-        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+        //string jumpLayers = "Ground";
+
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {return;}
+        //if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask(jumpLayers))) {return;}
 
         if (value.isPressed)
         {
-            myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
+            rb.velocity += new Vector2 (0f, jumpSpeed);
         }
     }
 
     void ClimbLadder()
     {
-        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
-        {
-            myRigidbody.gravityScale = gravityDefault;
-            return;
-        }
+        rb.gravityScale = gravityDefault;
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {return;}
 
-        Vector2 climbVelocity = new Vector2 (myRigidbody.velocity.x, moveInput.y * climbSpeed);
-        myRigidbody.velocity = climbVelocity;
-        myRigidbody.gravityScale = 0f;
+        bool ladderTouch = false;
+        bool upPress = false;
+
+        if (myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {ladderTouch = true;}
+        if (moveInput.y > Mathf.Epsilon) {upPress = true;}
+        if (ladderTouch && upPress) {ladderLatch = true;}
+
+        if ((ladderLatch && moveInput.y < 0) || (!ladderTouch)) {ladderLatch = false;}
+
+        if (ladderLatch)
+        {
+            Vector2 climbVelocity = new Vector2(rb.velocity.x, moveInput.y * climbSpeed);
+            rb.velocity = climbVelocity;
+            rb.gravityScale = 0f;
+        }
     }
 
     void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // true when player is moving R/L
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon; // true when player is moving R/L
 
         if (playerHasHorizontalSpeed) // only flip when moving
         {
-            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
         }
     }
 
